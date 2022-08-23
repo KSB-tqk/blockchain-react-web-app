25d1e5dc828e23d734d61de8bfa02e4ba294d060
Khanh Tran <khanhsbtqk@gmail.com>
2022-08-23 13:17:34 +0700

    init blockchain - react web-app

---
+++ smartcontract/contracts/MarketSentiment.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+contract marketSentiment {
+    address public owner;
+    string[] public ticketsArray;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct ticker {
+        bool exists;
+        uint256 up;
+        uint256 down;
+        mapping(address => bool) Voters;
+    }
+
+    event tickerupdated(uint256 up, uint256 down, address voter, string ticket);
+
+    mapping(string => ticker) private Tickers;
+
+    function addTicker(string memory _ticker) public {
+        require(msg.sender == owner, "Only the owner can create tickers");
+        ticker storage newTicker = Tickers[_ticker];
+        newTicker.exists = true;
+        ticketsArray.push(_ticker);
+    }
+}
